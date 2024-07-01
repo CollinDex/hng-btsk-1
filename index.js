65d24cc272aac802de04c5ace5ff6d7941815cb0
CollinDex <obetscollins@gmail.com>
2024-07-01 11:38:02 +0100

    Api changes for geolocation

--- index.js
+++ index.js
@@ -28,19 +28,17 @@ app.get('/api/hello/:visitor_name', async (req, res) => {
     try {
         const { visitor_name } = req.params;
 
-        //Get Client IP
-        const locationResponse = await axios.get(`${baseUrl}/ip.json?key=3ee659f3b5ce45a1b0681059240107&q=auto:ip`);
-        const { ip } = locationResponse.data;
-        console.log(ip);
-
+        //Get Client IP and CITY
+        const locationResponse = await axios.get(`http://ip-api.com/json/`);
+        const { query: ip, city: city } = locationResponse.data;
+        
+        console.log("IP ADRESS", ip);
         console.log("API_KEY", API_KEY);        
 
         //Get location from geolocation api
-        const weatherResponse = await axios.get(`${baseUrl}/current.json?key=3ee659f3b5ce45a1b0681059240107&q=${ip}`);
-        const { location, current } = weatherResponse.data;
-        const { name: city } = location;
+        const weatherResponse = await axios.get(`${baseUrl}/current.json?key=3ee659f3b5ce45a1b0681059240107&q=${city}`);
+        const { current } = weatherResponse.data;
         const { temp_c: temperature } = current;
-
         const greeting = `Hello, ${visitor_name}! The temperature is ${temperature} degrees Celsius in ${city}`;
 
         // Send the response
