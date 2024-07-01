528c8cc4bc4d5b29f6b1257a8605a882dd0a58e6
CollinDex <obetscollins@gmail.com>
2024-07-01 11:21:55 +0100

    Api changes

--- index.js
+++ index.js
@@ -29,27 +29,23 @@ app.get('/api/hello/:visitor_name', async (req, res) => {
         const { visitor_name } = req.params;
 
         //Get Client IP
-        let client_ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        const locationResponse = await axios.get(`${baseUrl}/ip.json?key=3ee659f3b5ce45a1b0681059240107&q=auto:ip`);
+        const { ip } = locationResponse.data;
+        console.log(ip);
 
-        //Clean up IPv6 address format
-        if (client_ip.includes('::ffff:')) {
-            client_ip = client_ip.split('::ffff:')[1];
-        };
-
-        console.log("API_KEY", API_KEY);
+        console.log("API_KEY", API_KEY);        
 
         //Get location from geolocation api
-        const weatherResponse = await axios.get(`${baseUrl}/current.json?key=3ee659f3b5ce45a1b0681059240107&q=auto:ip`);
+        const weatherResponse = await axios.get(`${baseUrl}/current.json?key=3ee659f3b5ce45a1b0681059240107&q=${ip}`);
         const { location, current } = weatherResponse.data;
         const { name: city } = location;
         const { temp_c: temperature } = current;
-
 
         const greeting = `Hello, ${visitor_name}! The temperature is ${temperature} degrees Celsius in ${city}`;
 
         // Send the response
         res.status(200).json({
-            client_ip: client_ip,
+            client_ip: ip,
             location: city,
             greeting: greeting
         });
